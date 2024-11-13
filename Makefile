83bce7cde3761b157841509c2832fe502b6a4a39
Malodel2 <malodel2@etsinf.upv.es>
2024-11-13 17:07:51 +0100

    trabajo completo

---
+++ Makefile
@@ -0,0 +1,57 @@
+bin/testListArray: testListArray.cpp ListArray.h List.h
+	mkdir -p bin
+	g++ -o bin/testListArray testListArray.cpp ListArray.h
+
+bin/testListLinked: testListLinked.cpp ListLinked.h List.h
+	mkdir -p bin
+	g++ -o bin/testListLinked testListLinked.cpp ListLinked.h
+
+bin/testNode: testNode.cpp Node.h
+	mkdir -p bin
+	g++ -o bin/testNode testNode.cpp Node.h
+
+Point2D.o: Point2D.h Point2D.cpp
+	g++ -c Point2D.cpp
+
+bin/testPoint2D: testPoint2D.cpp Point2D.o
+	g++ -c testPoint2D.cpp
+	mkdir -p bin
+	g++ -o bin/testPoint2D testPoint2D.o Point2D.o
+Shape.o: Shape.h Shape.cpp Point2D.h
+	g++ -c Shape.cpp
+
+Circle.o: Circle.h Circle.cpp Shape.h
+	g++ -c Circle.cpp
+
+bin/testCircle: testCircle.cpp Circle.o Shape.o Point2D.o
+	g++ -c testCircle.cpp
+	mkdir -p bin
+	g++ -o bin/testCircle testCircle.o Circle.o Shape.o Point2D.o
+
+Rectangle.o: Rectangle.h Rectangle.cpp Shape.h
+	g++ -c Rectangle.cpp
+
+bin/testRectangle: testRectangle.cpp Rectangle.o Shape.o Point2D.o
+	g++ -c testRectangle.cpp
+	mkdir -p bin
+	g++ -o bin/testRectangle testRectangle.o Rectangle.o Shape.o Point2D.o
+
+Square.o: Square.h Square.cpp Rectangle.h
+	g++ -c Square.cpp
+
+bin/testSquare: testSquare.cpp Square.o Rectangle.o Shape.o Point2D.o
+	g++ -c testSquare.cpp
+	mkdir -p bin
+	g++ -o bin/testSquare testSquare.o Square.o Rectangle.o Shape.o Point2D.o
+
+Drawing.o: Drawing.h Drawing.cpp ListArray.h
+	g++ -c Drawing.cpp
+
+bin/testDrawing: testDrawing.cpp Drawing.o Square.o Rectangle.o Circle.o Shape.o Point2D.o
+	mkdir -p bin
+	g++ -o bin/testDrawing testDrawing.cpp Drawing.o Square.o Rectangle.o Circle.o Shape.o Point2D.o
+
+clean:
+	rm -r *.o *.gch bin
+
+
---
+++ Square.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "Square.h"
+
+using namespace std;
+
+bool check(Point2D* vertices){
+    bool s = false;
+	if((Point2D::distance(vertices[0],vertices[1]) == Point2D::distance(vertices[1],vertices[2])) 
+	&& (Point2D::distance(vertices[1],vertices[2]) == Point2D::distance(vertices[2],vertices[3]))
+    && (Point2D::distance(vertices[1],vertices[2]) == Point2D::distance(vertices[0],vertices[3]))) s = true;
+	return s;
+} 
+
+ostream& operator<<(ostream &out, const Square &square){
+    out << "[Square: color = " << square.color << "; " ;
+    for(int i = 0; i < 4; i++)out << "v" << i << "= " << "(" << square.vs[i].x << "," << square.vs[i].y << ") ";
+	out << "]" << endl;
+	return out;
+} 
+
+void Square::print(){
+    cout << *this;
+} 
+void Square::set_vertices(Point2D* vertices){
+
+    bool s = false;
+	if((Point2D::distance(vertices[0],vertices[1]) == Point2D::distance(vertices[1],vertices[2])) 
+	&& (Point2D::distance(vertices[1],vertices[2]) == Point2D::distance(vertices[2],vertices[3]))
+    && (Point2D::distance(vertices[1],vertices[2]) == Point2D::distance(vertices[0],vertices[3]))) s = true;
+
+	if(s)vs = vertices;
+	else throw invalid_argument("Los vértices no conforman un cuadrado");
+} 
+
+Square::Square():Rectangle(){
+    Point2D* vertices = new Point2D[4];
+    vertices[0] = Point2D(-1,1);
+    vertices[1] = Point2D(1,1);
+    vertices[2] = Point2D(1,-1);
+    vertices[3] = Point2D(-1,-1);
+	Rectangle::set_vertices(vertices); 
+} 
+Square::Square(string color, Point2D* vertices){
+    bool s = false;
+	if((Point2D::distance(vertices[0],vertices[1]) == Point2D::distance(vertices[1],vertices[2])) 
+	&& (Point2D::distance(vertices[1],vertices[2]) == Point2D::distance(vertices[2],vertices[3]))
+    && (Point2D::distance(vertices[1],vertices[2]) == Point2D::distance(vertices[0],vertices[3]))) s = true;
+
+    if(s){
+        vs = vertices;
+        this->color = color;
+    } 
+	else throw invalid_argument("Los vértices no conforman un cuadrado");
+} 
+
+    
+char Square::type() {
+	return 's';
+} 
+
